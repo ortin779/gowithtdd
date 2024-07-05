4502576472817da6f93a705e47bdd467ca740ebf
Naresh <chakalinaresh1729@gmail.com>
2024-07-06 00:26:12 +0530

    handle empty slice case when checking tail sum

--- arraysandslices/sum.go
+++ arraysandslices/sum.go
@@ -15,3 +15,16 @@ func SumAll(numsCol ...[]int) []int {
 	}
 	return sums
 }
+
+func SumAllTails(numsCol ...[]int) []int {
+	var sums []int
+	for _, nums := range numsCol {
+		if len(nums) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := nums[1:]
+			sums = append(sums, Sum(tail))
+		}
+	}
+	return sums
+}
--- arraysandslices/sum_test.go
+++ arraysandslices/sum_test.go
@@ -2,6 +2,7 @@ package arraysandslices
 
 import (
 	"reflect"
+	"slices"
 	"testing"
 )
 
@@ -32,8 +33,39 @@ func TestSumAll(t *testing.T) {
 	}
 }
 
+func TestSumAllTails(t *testing.T) {
+	checkSums := func(t testing.TB, expected, got []int) {
+		t.Helper()
+
+		if !slices.Equal(got, expected) {
+			t.Errorf("expected %v but got %v", expected, got)
+		}
+	}
+
+	t.Run("make the sum of some slices", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2}, []int{1, 2, 3})
+		expect := []int{2, 5}
+
+		checkSums(t, expect, got)
+	})
+
+	t.Run("safely sum empty slices", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{3, 4, 5})
+		want := []int{0, 9}
+
+		checkSums(t, want, got)
+
+	})
+}
+
 func BenchmarkSumAll(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		SumAll([][]int{{1, 2, 3}, {2, 3}}...)
+	}
+}
+
+func BenchmarkSumAllTails(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		SumAllTails([][]int{{1, 2, 3}, {2, 3}}...)
 	}
 }
