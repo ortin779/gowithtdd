824fb2b7b5889c00e77c53843fd071534981373c
Naresh <chakalinaresh1729@gmail.com>
2024-07-06 22:24:44 +0530

    explore concurrency and channels in go

---
+++ concurrency/websites_test.go
@@ -0,0 +1,50 @@
+package concurrency
+
+import (
+	"maps"
+	"testing"
+	"time"
+)
+
+func mockWebsiteCheker(url string) bool {
+	if url == "waat://furhurterwe.geds" {
+		return false
+	}
+	return true
+}
+
+func slowStubWebsiteChecker(_ string) bool {
+	time.Sleep(20 * time.Millisecond)
+	return true
+}
+
+func TestCheckWebsites(t *testing.T) {
+	websites := []string{
+		"http://google.com",
+		"http://blog.gypsydave5.com",
+		"waat://furhurterwe.geds",
+	}
+
+	expected := map[string]bool{
+		"http://google.com":          true,
+		"http://blog.gypsydave5.com": true,
+		"waat://furhurterwe.geds":    false,
+	}
+
+	got := CheckWebsites(mockWebsiteCheker, websites)
+
+	if !maps.Equal(expected, got) {
+		t.Errorf("expected %v, but got %v", expected, got)
+	}
+}
+
+func BenchmarkCheckWebsites(b *testing.B) {
+	urls := make([]string, 100)
+	for i := 0; i < len(urls); i++ {
+		urls[i] = "a url"
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		CheckWebsites(slowStubWebsiteChecker, urls)
+	}
+}
