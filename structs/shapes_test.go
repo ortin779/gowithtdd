d9d6e9bda57f0311fd51e92867b11f7c57038f33
Naresh <chakalinaresh1729@gmail.com>
2024-07-06 13:54:02 +0530

    refactor tests to table driver testing

--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -19,25 +19,31 @@ func TestPerimeter(t *testing.T) {
 
 func TestArea(t *testing.T) {
 
-	checkArea := func(t testing.TB, shape Shape, expected float64) {
-		t.Helper()
+	areaTests := []struct {
+		shape    Shape
+		expected float64
+	}{
+		{
+			Rectangle{
+				Height: 12.0,
+				Width:  6.0,
+			},
+			72.0,
+		},
+		{
+			Circle{
+				Raidus: 10.0,
+			},
+			314.1592653589793,
+		},
+	}
 
-		got := shape.Area()
-		if expected != got {
-			t.Errorf("expected %.2f, but got %.2f", expected, got)
+	for _, tt := range areaTests {
+		got := tt.shape.Area()
+
+		if got != tt.expected {
+			t.Errorf("expected %g, but got %g", tt.expected, got)
 		}
 	}
-	t.Run("rectangles area", func(t *testing.T) {
-		rect := Rectangle{
-			Height: 12.0,
-			Width:  6.0,
-		}
-		checkArea(t, rect, 72.0)
-	})
 
-	t.Run("circle area", func(t *testing.T) {
-		c := Circle{Raidus: 10.0}
-		checkArea(t, c, 314.1592653589793)
-
-	})
 }
