3b7740974f3bf8c41d7ac66c54e8913bda86183e
Naresh <chakalinaresh1729@gmail.com>
2024-07-07 11:34:29 +0530

    explore reflect package to work with dynamic types

---
+++ reflection/reflection.go
@@ -0,0 +1,31 @@
+package reflection
+
+import (
+	"reflect"
+)
+
+func Walk(x interface{}, fn func(field string)) {
+	val := getValue(x)
+
+	switch val.Kind() {
+	case reflect.Struct:
+		for i := 0; i < val.NumField(); i++ {
+			Walk(val.Field(i).Interface(), fn)
+		}
+	case reflect.Slice:
+		for i := 0; i < val.Len(); i++ {
+			Walk(val.Index(i).Interface(), fn)
+		}
+	case reflect.String:
+		fn(val.String())
+	}
+}
+
+func getValue(x interface{}) reflect.Value {
+	val := reflect.ValueOf(x)
+
+	if val.Kind() == reflect.Pointer {
+		val = val.Elem()
+	}
+	return val
+}
